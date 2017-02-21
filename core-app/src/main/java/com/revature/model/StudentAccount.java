05fe2ed4e6e4f002b1b2174903e3168361b88cce
AiswaryaRavi <aiswaryaravindran240@gmail.com>
2017-02-21 18:23:21 +0530

    Updated

---
+++ core-app/src/main/java/com/revature/model/StudentAccount.java
@@ -0,0 +1,29 @@
+package com.revature.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "student_account")
+
+public class StudentAccount {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	@OneToOne
+	@JoinColumn(name = "STUDENT_ID", unique = true)
+	private Student studentId;
+
+	private String username;
+
+	private String password;
+}
---
+++ core-app/src/main/java/com/revature/model/StudentQuiz.java
@@ -0,0 +1,43 @@
+package com.revature.model;
+import java.sql.Time;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Data;
+@Data
+@Entity
+@Table(name = "student_quizes")
+
+public class StudentQuiz {
+	 @Id
+	  @GeneratedValue(strategy = GenerationType.AUTO)
+	  private Integer id;
+	  @ManyToOne
+	    @JoinColumn(name = "STUDENT_ID")
+	  private Student studentId;
+	  @ManyToOne
+	    @JoinColumn(name = "QUIZ_ID")
+	  private Quiz quizId;
+	  @Column(name = "STARTED_ON")
+	  @Temporal(TemporalType.TIME)
+	 private Time startedOn;
+	  @Column(name ="COMPLETED_ON")
+	  @Temporal(TemporalType.TIME)
+	 private Time completedOn;
+	  @ManyToOne
+	    @JoinColumn(name = "STATUS_ID")
+	  private SeedStatus statusId=1;
+	  
+
+
+
+}
